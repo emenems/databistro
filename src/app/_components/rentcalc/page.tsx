51a1044c355268206eee97a16de2c187d5815338
Michal Mikolaj <michal.mikolaj@telekom.com>
2024-07-21 17:43:06 +0200

    add api for stock returns

--- src/app/_components/rentcalc/page.tsx
+++ src/app/_components/rentcalc/page.tsx
@@ -1,11 +1,13 @@
 
 import RentCalc from "@/app/_components/rentcalc/calculate-rent";
+import { getStockReturnSeries } from "@/lib/api";
 
 export default async function PageCharts({ }) {
+    const data = await getStockReturnSeries();
     return (
         <>
             <div className="container flex flex-row items-center">
-                <RentCalc />
+                <RentCalc stockData={data}/>
             </div>
         </>
     );
