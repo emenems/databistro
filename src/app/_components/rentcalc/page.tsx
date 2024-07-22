da9893e675ca739e01bc874693d960360fc5dc9f
Michal Mikolaj <michal.mikolaj@telekom.com>
2024-07-22 08:02:10 +0200

    use demo data for faster load

--- src/app/_components/rentcalc/page.tsx
+++ src/app/_components/rentcalc/page.tsx
@@ -1,13 +1,14 @@
 
 import RentCalc from "@/app/_components/rentcalc/calculate-rent";
-import { getStockReturnSeries } from "@/lib/api";
+// import { getStockReturnSeries } from "@/lib/api";
+import { stockReturnSeriesDemo } from "./utils";
 
 export default async function PageCharts({ }) {
-    const data = await getStockReturnSeries("^DJI", "2004-07-01", "2024-07-19");
+    // const data = await getStockReturnSeries("^DJI", "2004-07-01", "2024-07-19");
     return (
         <>
             <div className="container flex flex-row items-center">
-                <RentCalc stockData={data}/>
+                <RentCalc stockData={stockReturnSeriesDemo}/>
             </div>
         </>
     );
